ecad06e1c36556e3d1fdde95e4b351adcc9468d2
Rubeun Tan <github@rubeun.com>
2018-10-25 01:59:27 -0700

    Tweet checks if it is replying to another Tweet

--- twatter/src/components/Tweet.js
+++ twatter/src/components/Tweet.js
@@ -4,7 +4,14 @@ import { formatTweet } from '../utils/helpers';
 
 class Tweet extends Component {
   render() {
+    const { tweet } = this.props;
+    
+    if (tweet === null) {
+      return <p>This Tweet doesn't exist</p>
+    }
+
     console.log(this.props);
+    
     return (
       <div className='tweet'>
 
@@ -13,14 +20,18 @@ class Tweet extends Component {
   }
 }
 
-// Tweet needs authedUser, users, tweets from store. Tweet is also passed in id as prop (from parent)
+// Tweet needs authedUser, users, tweets from store. Tweet is also passed in id as prop (of Tweet to be displayed)
 // note: formatTweet(tweet, author, authedUser, parentTweet) and returns formatted tweet in an object
 function mapStateToProps({authedUser, users, tweets}, { id }) {
   const tweet = tweets[id];
+  // check that the tweet is replying to another tweet
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+    tweet: tweet 
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null
   }
 }
 
