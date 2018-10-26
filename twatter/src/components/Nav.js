c11fd0737d72cb221dd33f17ca4c238ae45865f0
Rubeun Tan <github@rubeun.com>
2018-10-25 22:50:27 -0700

    Added Twatter Icon just because

--- twatter/src/components/Nav.js
+++ twatter/src/components/Nav.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 export default function Nav() {
   return (
     <nav className='nav'>
+      <img className='twatter-logo' width="50" src="https://icon2.kisspng.com/20180203/wze/kisspng-bird-logo-euclidean-vector-icon-twitter-transparent-background-5a75930155d476.5647598015176547853516.jpg" alt="Twatter Logo" />
       <ul>
         <li>
           <NavLink to='/' exact activeClassName='active'>
