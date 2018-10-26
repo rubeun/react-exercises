832152b3d6eb70a4e1690f178a8448f45486f57f
Rubeun Tan <github@rubeun.com>
2018-10-26 00:16:09 -0700

    Initial Commit. Basic framework built

---
+++ would-you-rather/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        Would You Rather?
+      </div>
+    )
+  }
+}
+
+export default App;
